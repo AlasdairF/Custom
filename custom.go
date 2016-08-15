823a7f2716ef6295f192450f7888c18ce939e52b
AlasdairF <AlasdairF@users.noreply.github.com>
2016-08-15 12:23:15 +0700

    Added (*Buffer) ToReader

--- custom.go
+++ custom.go
@@ -830,6 +830,11 @@ func NewBuffer(l int) *Buffer {
 	}
 }
 
+// Wraps the written data in a new reader. The original Buffer will need to be closed after all the reading is done.
+func (w *Writer) Reader() *BytesReader {
+	return NewBytesReader(w.data[0:w.length])
+}
+
 // Reads all of r into the buffer
 func (w *Buffer) ReadFrom(r io.Reader) (int, error) {
 	var i, n int
